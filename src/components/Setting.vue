64946636c01421a332971edf6e5edcb77ae11361
Terra <bieyuanxi@163.com>
2024-01-08 17:46:27 +0800

    setting page refactor

--- src/components/Setting.vue
+++ src/components/Setting.vue
@@ -1,18 +1,18 @@
 <template>
     <n-layout has-sider>
-        <n-layout-sider bordered content-style="padding: 12px;">
+        <n-layout-sider bordered content-style="padding: 0px" width="160px">
             <n-space style="margin-bottom: 12px">
                 <n-switch v-model:value="showRail" /> 展示轨道
-                <n-switch v-model:value="showBackground" /> 展示背景
+                <!-- <n-switch v-model:value="showBackground" /> 展示背景 -->
             </n-space>
+            
             <n-anchor :show-rail="showRail" :show-background="showBackground">
-                <n-anchor-link title="演示" href="#演示">
-                    <n-anchor-link title="Theme" href="#basic.vue" />
-                    <n-anchor-link title="固定" href="#affix.vue" />
-                    <n-anchor-link title="滚动到" href="#Forbidden.vue" />
+
+                <n-anchor-link v-for="option in menuOptions" :title="option.title" :href="option.href">
+                    <n-anchor-link v-for="sub_option in option.children" :title="sub_option.title" :href="sub_option.href" />
                 </n-anchor-link>
-                <n-anchor-link title="About" href="#setting/about" />
-                
+
+
                 <!-- <n-anchor-link title="Debug" href="#setting/debug" /> -->
             </n-anchor>
         </n-layout-sider>
@@ -26,9 +26,30 @@
   
 <script setup>
 import { defineComponent, ref } from "vue";
-import About from "./About.vue"
+import { NAnchorLink } from "naive-ui"
 
 const showRail = ref(true);
 const showBackground = ref(true);
+
+// todo: a recursive menu
+const menuOptions = [
+    {
+        title: "Config",
+        href: "#setting",
+        children: [
+            {
+                title: "t11",
+                href: "#setting",
+            },
+        ],
+    },
+    {
+        title: "About",
+        href: "#setting/about",
+    },
+];
+
+
+
 
 </script>
