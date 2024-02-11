6a5f5b361fa30b6b0d6971de0afa0896f195ded6
Terra <bieyuanxi@163.com>
2024-02-11 11:07:24 +0800

    impl chart

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -24,8 +24,11 @@ import {
     LegendComponent,
 } from 'echarts/components';
 import VChart, { THEME_KEY } from 'vue-echarts';
-import { ref, provide } from 'vue';
+import { ref, provide, onMounted, reactive } from 'vue';
 import { NGrid, NGi } from "naive-ui"
+
+import { getDb } from '../db';
+import { gacha_type } from '../mihoyo_api';
 
 use([
     CanvasRenderer,
@@ -37,9 +40,34 @@ use([
 
 provide(THEME_KEY, 'white');
 
-const data = [
-    "a", "b", "c"
-]
+const data = ref([
+    "actor_msgTODO", "weapon_msgTODO", "normal_msgTODO"
+])
+
+const actor_data = ref([
+    // { value: 335, name: '5star' },
+    // { value: 10, name: '4star_actor' },
+    // { value: 30, name: '4star_weapon' },
+])
+const weapon_data = ref([])
+const normal_data = ref([])
+
+const pie_actor_data = reactive({
+    legend_data: [],
+    series_data: []
+})
+
+const pie_weapon_data = reactive({
+    legend_data: [],
+    series_data: []
+})
+
+const pie_normal_data = reactive({
+    legend_data: [],
+    series_data: []
+})
+
+summary()
 
 const options = ref([
     {
@@ -54,21 +82,15 @@ const options = ref([
         legend: {
             orient: 'vertical',
             left: 'left',
-            data: ['5star', '4star_actor', '4star_weapon', '3star', 'Search Engines'],
+            data: pie_actor_data.legend_data,
         },
         series: [
             {
-                name: 'Traffic Sources',
+                name: 'actor',
                 type: 'pie',
                 radius: '55%',
                 center: ['50%', '60%'],
-                data: [
-                    { value: 335, name: '5star' },
-                    { value: 310, name: '4star_actor' },
-                    { value: 234, name: '4star_weapon' },
-                    { value: 135, name: '3star' },
-                    { value: 1548, name: 'Search Engines' },
-                ],
+                data: pie_actor_data.series_data,
                 emphasis: {
                     itemStyle: {
                         shadowBlur: 10,
@@ -91,21 +113,15 @@ const options = ref([
         legend: {
             orient: 'vertical',
             left: 'left',
-            data: ['Direct', 'Email', 'Ad Networks', 'Video Ads', 'Search Engines'],
+            data: pie_weapon_data.legend_data,
         },
         series: [
             {
-                name: 'Traffic Sources',
+                name: 'weapon',
                 type: 'pie',
                 radius: '55%',
                 center: ['50%', '60%'],
-                data: [
-                    { value: 335, name: 'Direct' },
-                    { value: 310, name: 'Email' },
-                    { value: 234, name: 'Ad Networks' },
-                    { value: 135, name: 'Video Ads' },
-                    { value: 1548, name: 'Search Engines' },
-                ],
+                data: pie_weapon_data.series_data,
                 emphasis: {
                     itemStyle: {
                         shadowBlur: 10,
@@ -128,21 +144,15 @@ const options = ref([
         legend: {
             orient: 'vertical',
             left: 'left',
-            data: ['Direct', 'Email', 'Ad Networks', 'Video Ads', 'Search Engines'],
+            data: pie_normal_data.legend_data,
         },
         series: [
             {
-                name: 'Traffic Sources',
+                name: 'Normal',
                 type: 'pie',
                 radius: '55%',
                 center: ['50%', '60%'],
-                data: [
-                    { value: 335, name: 'Direct' },
-                    { value: 310, name: 'Email' },
-                    { value: 234, name: 'Ad Networks' },
-                    { value: 135, name: 'Video Ads' },
-                    { value: 1548, name: 'Search Engines' },
-                ],
+                data: pie_normal_data.series_data,
                 emphasis: {
                     itemStyle: {
                         shadowBlur: 10,
@@ -154,6 +164,117 @@ const options = ref([
         ],
     }
 ]);
+
+
+
+async function summary() {
+    await summaryActor();
+    await summaryWeapon();
+    await summaryNormal();
+    await query();
+}
+
+async function summaryActor() {
+    const type = '301'
+    const db = await getDb();
+
+    // pie_data.legend_data.push('5star', '4star_actor', '4star_weapon')
+
+    {
+        let sql = `SELECT COUNT(*) AS count FROM gacha_log WHERE gacha_type=${type} AND rank_type='5'`;
+        const result = await db.select(sql);
+
+        pie_actor_data.series_data.push({ value: result[0].count, name: "5star" })
+    }
+
+    {
+        let item = '武器'   //FIX ME: i18n
+        let sql = `SELECT COUNT(*) AS count FROM gacha_log WHERE gacha_type=${type} AND rank_type='4' AND item_type='${item}'`;
+        const result = await db.select(sql);
+
+        pie_actor_data.series_data.push({ value: result[0].count, name: "4star_weapon" })
+    }
+
+    {
+        let item = '角色'   //FIX ME: i18n
+        let sql = `SELECT COUNT(*) AS count FROM gacha_log WHERE gacha_type=${type} AND rank_type='4' AND item_type='${item}'`;
+        const result = await db.select(sql);
+
+        pie_actor_data.series_data.push({ value: result[0].count, name: "4star_actor" })
+    }
+
+    pie_actor_data.legend_data.push('5star', '4star_actor', '4star_weapon')
+
+    console.log(pie_actor_data)
+}
+
+async function summaryWeapon() {
+    const type = '302'
+    const db = await getDb();
+
+    // pie_data.legend_data.push('5star', '4star_actor', '4star_weapon')
+
+    {
+        let sql = `SELECT COUNT(*) AS count FROM gacha_log WHERE gacha_type=${type} AND rank_type='5'`;
+        const result = await db.select(sql);
+
+        pie_weapon_data.series_data.push({ value: result[0].count, name: "5star" })
+    }
+
+    {
+        let item = '武器'   //FIX ME: i18n
+        let sql = `SELECT COUNT(*) AS count FROM gacha_log WHERE gacha_type=${type} AND rank_type='4' AND item_type='${item}'`;
+        const result = await db.select(sql);
+
+        pie_weapon_data.series_data.push({ value: result[0].count, name: "4star_weapon" })
+    }
+
+    {
+        let item = '角色'   //FIX ME: i18n
+        let sql = `SELECT COUNT(*) AS count FROM gacha_log WHERE gacha_type=${type} AND rank_type='4' AND item_type='${item}'`;
+        const result = await db.select(sql);
+
+        pie_weapon_data.series_data.push({ value: result[0].count, name: "4star_actor" })
+    }
+
+    pie_weapon_data.legend_data.push('5star', '4star_actor', '4star_weapon')
+}
+
+async function summaryNormal() {
+    const type = '200'
+    const db = await getDb();
+    const item_type = ['角色', '武器'];
+    const rank_type = ['4', '5'];
+
+    for (let item of item_type) {
+        for (let rank of rank_type) {
+            let sql = `SELECT COUNT(*) AS count FROM gacha_log WHERE gacha_type=${type} AND rank_type='${rank}' AND item_type='${item}'`;
+            const result = await db.select(sql);
+            console.log(sql)
+            console.log(result)
+            pie_normal_data.series_data.push({ value: result[0].count, name: `${rank}star_${item}` })
+            pie_normal_data.legend_data.push(`${rank}star_${item}`)
+        }
+    }
+}
+
+async function query() {
+    // const item_type = ['角色', '武器'];
+    // const rank_type = ['4', '5'];
+    const db = await getDb();
+    const type = '5'
+    const gacha_type = ['200', '301', '302'];
+    for(let type of gacha_type) {
+        let sql = `SELECT * FROM gacha_log WHERE gacha_type=${type} AND rank_type='5'`;
+        const result = await db.select(sql);
+        console.log(result)
+        for(let one of result) {
+            data.value.push(one.name);
+        }
+    }
+    
+    
+}
 </script>
 
 <style scoped>
